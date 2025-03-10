e1e482d23c66e606c40b8641cc82b48da083ae98
Ahasan Ullah <ahasanullah451@gmail.com>
2025-03-11 02:39:25 +0600

    get method for all users added

--- index.js
+++ index.js
@@ -50,6 +50,11 @@ async function run() {
       res.send(result);
     })
 
+    app.get('/users', async (req,res)=>{
+      const result= await users.find().toArray();
+      res.send(result);
+    })
+
     //getting menu
     app.get('/menu',async (req,res)=>{
       const result=await menu.find().toArray();
